d9b21930c787308723e10a2a26e9ae6b2c159a94
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2019-05-11 19:24:44 +0300

    feat: #03 add implicit intent

--- chapter03/MyMessenger/app/src/main/java/by/yakivan/mymessenger/CreateMessageActivity.java
+++ chapter03/MyMessenger/app/src/main/java/by/yakivan/mymessenger/CreateMessageActivity.java
@@ -4,17 +4,31 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class CreateMessageActivity extends AppCompatActivity {
+    private EditText messageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_message);
+        messageView = findViewById(R.id.message);
     }
 
     public void onSendMessage(View view) {
         Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        String messageText = messageView.getText().toString();
+
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
         startActivity(intent);
+    }
+
+    public void onSendMessageEmail(View view) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, messageView.getText().toString());
+        Intent chooseIntent = Intent.createChooser(intent, this.getString(R.string.choserText));
+        startActivity(chooseIntent);
     }
 }
--- chapter03/MyMessenger/app/src/main/java/by/yakivan/mymessenger/ReceiveMessageActivity.java
+++ chapter03/MyMessenger/app/src/main/java/by/yakivan/mymessenger/ReceiveMessageActivity.java
@@ -2,12 +2,16 @@ package by.yakivan.mymessenger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ReceiveMessageActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_message);
+        TextView viewMessages = findViewById(R.id.receive_message);
+        viewMessages.setText(getIntent().getStringExtra(EXTRA_MESSAGE));
     }
 }
