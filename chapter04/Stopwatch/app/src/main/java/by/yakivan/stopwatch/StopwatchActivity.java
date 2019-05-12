66ee22c7d34d7fc771015a84ce054bd2196c53fe
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2019-05-12 16:56:42 +0300

    feat: #04 init

---
+++ chapter04/Stopwatch/app/src/main/java/by/yakivan/stopwatch/StopwatchActivity.java
@@ -0,0 +1,56 @@
+package by.yakivan.stopwatch;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class StopwatchActivity extends AppCompatActivity {
+    private int seconds = 0;
+    private boolean running;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+        runTimer();
+    }
+
+    public void onClickStart(View view) {
+        running = true;
+    }
+
+    public void onClickStop(View view) {
+        running = false;
+    }
+
+    public void onClickReset(View view) {
+        running = false;
+        seconds = 0;
+    }
+
+    private void runTimer() {
+        final TextView textView = findViewById(R.id.stopwatch_time);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+
+                String time = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, secs);
+                textView.setText(time);
+                if (running)
+                    seconds++;
+
+
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }
+}
