281741895cad631c20aff140c4f9a99b06fc041b
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2019-05-28 12:28:59 +0300

    feat: #05 add duck to interface

---
+++ chapter05/LayoutExamples/app/src/main/java/by/yakivan/layoutexamples/DuckActivity.java
@@ -0,0 +1,13 @@
+package by.yakivan.layoutexamples;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DuckActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_duck);
+    }
+}
