3bf629ea342f8abb88185e1ecf855e24b7dbdabe
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2019-05-28 21:51:14 +0300

    feat: #05 add ability to choose tea

--- chapter05/LayoutExamples/app/src/main/java/by/yakivan/layoutexamples/MainActivity.java
+++ chapter05/LayoutExamples/app/src/main/java/by/yakivan/layoutexamples/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +15,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onDuckLayout(View view) {
+        Toast.makeText(this, "Hello, I am a duck.", Toast.LENGTH_LONG).show();
         Intent intent = new Intent(this, DuckActivity.class);
         startActivity(intent);
+    }
+
+    public void onTeaLayout(View view) {
+        Toast.makeText(this, "Please, Choose drink.", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, TeaActivity.class);
+        startActivity(intent);
+
     }
 }
