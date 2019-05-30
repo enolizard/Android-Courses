fce79522a5ff8f68015ec1518e558ac967636714
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2019-05-30 20:39:33 +0300

    feat: #07 add detail of drink

--- chapter07/Starbuzz/app/src/main/java/by/yakivan/starbuzz/DrinkActivity.java
+++ chapter07/Starbuzz/app/src/main/java/by/yakivan/starbuzz/DrinkActivity.java
@@ -1,13 +1,35 @@
 package by.yakivan.starbuzz;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DrinkActivity extends AppCompatActivity {
 
+    public static final String EXTRA_DRINK_ID = "drinkId";
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink);
+
+        int drinkId = getIntent().getIntExtra(EXTRA_DRINK_ID, -1);
+
+        ImageView photo = findViewById(R.id.photo);
+        TextView name = findViewById(R.id.name);
+        TextView description = findViewById(R.id.description);
+
+        if (drinkId == -1) {
+            finish();
+        } else {
+            Drink drink = Drink.drinks[drinkId];
+            photo.setImageDrawable(getDrawable(drink.getImageResourseId()));
+            name.setText(drink.getName());
+            description.setText(drink.getDescription());
+        }
     }
 }
--- chapter07/Starbuzz/app/src/main/java/by/yakivan/starbuzz/DrinkCategoryActivity.java
+++ chapter07/Starbuzz/app/src/main/java/by/yakivan/starbuzz/DrinkCategoryActivity.java
@@ -1,7 +1,10 @@
 package by.yakivan.starbuzz;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -12,8 +15,20 @@ public class DrinkCategoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_category);
 
-        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Drink.drinks);
         ListView listDrinks = findViewById(R.id.list_drinsk);
+
+        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Drink.drinks);
+
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(DrinkCategoryActivity.this, DrinkActivity.class);
+                intent.putExtra(DrinkActivity.EXTRA_DRINK_ID, (int) id);
+                startActivity(intent);
+            }
+        };
+
         listDrinks.setAdapter(listAdapter);
+        listDrinks.setOnItemClickListener(itemClickListener);
     }
 }
