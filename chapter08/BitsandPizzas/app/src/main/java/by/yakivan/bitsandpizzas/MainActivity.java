5bad74ff75175dbe0f315271a220fa92dec71854
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2019-05-31 18:15:43 +0300

    feat: #08 add order activity and design main activity

--- chapter08/BitsandPizzas/app/src/main/java/by/yakivan/bitsandpizzas/MainActivity.java
+++ chapter08/BitsandPizzas/app/src/main/java/by/yakivan/bitsandpizzas/MainActivity.java
@@ -1,7 +1,11 @@
 package by.yakivan.bitsandpizzas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_create_order:
+                Intent intent = new Intent(this, OrderActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
