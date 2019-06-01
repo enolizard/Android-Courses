f5ebedd04eef2c821d3f61b20e5255a755daa71b
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2019-06-01 14:55:40 +0300

    feat: #09 add detail fragment to detail activity

---
+++ chapter09/Workout/app/src/main/java/by/yakivan/workout/WorkoutDetailFragment.java
@@ -0,0 +1,19 @@
+package by.yakivan.workout;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WorkoutDetailFragment extends Fragment {
+
+    public WorkoutDetailFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+}
