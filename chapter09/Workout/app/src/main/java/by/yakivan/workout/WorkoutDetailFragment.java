2dea9dcd03dd475585b78fd59a076e928c53cd11
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2019-06-01 15:48:38 +0300

    feat: #09 add to set data to the Detail Activity

--- chapter09/Workout/app/src/main/java/by/yakivan/workout/WorkoutDetailFragment.java
+++ chapter09/Workout/app/src/main/java/by/yakivan/workout/WorkoutDetailFragment.java
@@ -5,8 +5,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class WorkoutDetailFragment extends Fragment {
+    private int workoutId;
 
     public WorkoutDetailFragment() {
     }
@@ -15,5 +17,23 @@ public class WorkoutDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        View view = getView();
+        if (view != null) {
+            Workout workout = Workout.workouts[workoutId];
+            TextView titleView = view.findViewById(R.id.workoutTitle);
+            TextView descriptionView = view.findViewById(R.id.workoutDescription);
+
+            titleView.setText(workout.getName());
+            descriptionView.setText(workout.getDescription());
+        }
+    }
+
+    public void setWorkoutId(int workoutId) {
+        this.workoutId = workoutId;
     }
 }
