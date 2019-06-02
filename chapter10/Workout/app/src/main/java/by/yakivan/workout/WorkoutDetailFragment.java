8a260a461b01f8706f56bf8f2c5f1c02623378ff
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2019-06-02 21:56:17 +0300

    feat: #10 init

---
+++ chapter10/Workout/app/src/main/java/by/yakivan/workout/WorkoutDetailFragment.java
@@ -0,0 +1,40 @@
+package by.yakivan.workout;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class WorkoutDetailFragment extends Fragment {
+
+    private int workoutId;
+
+    public WorkoutDetailFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        View view = getView();
+        if (view != null) {
+            Workout workout = Workout.workouts[workoutId];
+            TextView titleView = view.findViewById(R.id.workoutTitle);
+            TextView descriptionView = view.findViewById(R.id.workoutDescription);
+
+            titleView.setText(workout.getName());
+            descriptionView.setText(workout.getDescription());
+        }
+    }
+
+    public void setWorkoutId(int workoutId) {
+        this.workoutId = workoutId;
+    }
+}
---
+++ chapter10/Workout/app/src/main/java/by/yakivan/workout/WorkoutListFragment.java
@@ -0,0 +1,48 @@
+package by.yakivan.workout;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class WorkoutListFragment extends ListFragment {
+
+    interface Listener {
+        void itemClicked(int id);
+    }
+
+    private Listener listener;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.listener = (Listener) context;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        String[] names = new String[Workout.workouts.length];
+        for (int i = 0; i < names.length; i++) {
+            names[i] = Workout.workouts[i].getName();
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                inflater.getContext(),
+                android.R.layout.simple_list_item_1,
+                names);
+        setListAdapter(adapter);
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        if (listener != null)
+            listener.itemClicked((int) id);
+    }
+}
