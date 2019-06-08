766cd0162f32141da8fe9ad10439318f3c3c1e3b
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2019-06-08 09:36:35 +0300

    feat: #13 init

---
+++ chapter13/BitsandPizzas/app/src/main/java/by/yakivan/bitsandpizzas/OrderActivity.java
@@ -0,0 +1,43 @@
+package by.yakivan.bitsandpizzas;
+
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void onClickDone(View view) {
+        String msg = "Hello, I'm a Snackbar!";
+        Snackbar snackbar = Snackbar.make(
+                findViewById(R.id.coordinator),
+                msg,
+                Snackbar.LENGTH_SHORT);
+        snackbar.setAction("Undo", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(
+                        OrderActivity.this,
+                        "Undone!",
+                        Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+        snackbar.show();
+    }
+}
---
+++ chapter13/BitsandPizzas/app/src/main/java/by/yakivan/bitsandpizzas/TopFragment.java
@@ -0,0 +1,19 @@
+package by.yakivan.bitsandpizzas;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TopFragment extends Fragment {
+
+    public TopFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_top, container, false);
+    }
+}
