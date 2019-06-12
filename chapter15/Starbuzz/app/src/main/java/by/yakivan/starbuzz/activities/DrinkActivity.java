f5fb94fbdf6f522b55c6779e362738f2a5e7b525
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2019-06-12 08:23:47 +0300

    feat: #15 add DB Opener Helper

--- chapter15/Starbuzz/app/src/main/java/by/yakivan/starbuzz/activities/DrinkActivity.java
+++ chapter15/Starbuzz/app/src/main/java/by/yakivan/starbuzz/activities/DrinkActivity.java
@@ -1,9 +1,7 @@
 package by.yakivan.starbuzz.activities;
 
-import android.annotation.TargetApi;
-import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
 
