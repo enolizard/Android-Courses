bccd648635dfe1c6d12e492618dfbfea880a351c
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2019-06-13 09:49:12 +0300

    feat: #15 add to update DB

--- chapter15/Starbuzz/app/src/main/java/by/yakivan/starbuzz/database/DatabaseHelper.java
+++ chapter15/Starbuzz/app/src/main/java/by/yakivan/starbuzz/database/DatabaseHelper.java
@@ -17,22 +17,31 @@ class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE DRINK(" +
-                "_id INTEGER PRIMARY KEY AUTOINCREMENT," +
-                "NAME TEXT," +
-                "DESCRIPTION TEXT," +
-                "IMAGE_RESOURCE_ID INTEGER)");
-        insertDrink(db, "Latte", "Espresso and steamed milk",
-                R.drawable.latte);
-        insertDrink(db, "Cappuccino", "Espresso, hot milk and steamed-milk foam",
-                R.drawable.cappuccino);
-        insertDrink(db, "Filter", "Our best drip coffee",
-                R.drawable.filter);
+        updateDatabase(db, 0, DB_VERSION);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        updateDatabase(db, oldVersion, newVersion);
+    }
 
+    private void updateDatabase(SQLiteDatabase db, int oldVersion, int newVersion)  {
+        if (oldVersion < 1) {
+            db.execSQL("CREATE TABLE DRINK(" +
+                    "_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    "NAME TEXT," +
+                    "DESCRIPTION TEXT," +
+                    "IMAGE_RESOURCE_ID INTEGER)");
+            insertDrink(db, "Latte", "Espresso and steamed milk",
+                    R.drawable.latte);
+            insertDrink(db, "Cappuccino", "Espresso, hot milk and steamed-milk foam",
+                    R.drawable.cappuccino);
+            insertDrink(db, "Filter", "Our best drip coffee",
+                    R.drawable.filter);
+        }
+        if (oldVersion < 2) {
+            db.execSQL("ALTER TABLE DRINK ADD COLUMN FAVORITE NUMERIC;");
+        }
     }
 
     private static void insertDrink(SQLiteDatabase db,
