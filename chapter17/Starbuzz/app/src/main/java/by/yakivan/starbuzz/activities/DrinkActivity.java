8e5ffffd6f3bd8abfe2999b3d5a4cad1ee180bb6
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2019-06-19 08:13:19 +0300

    feat: #17 init

---
+++ chapter17/Starbuzz/app/src/main/java/by/yakivan/starbuzz/activities/DrinkActivity.java
@@ -0,0 +1,60 @@
+package by.yakivan.starbuzz.activities;
+
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import by.yakivan.starbuzz.Drink;
+import by.yakivan.starbuzz.R;
+import by.yakivan.starbuzz.database.DatabaseHelper;
+
+public class DrinkActivity extends AppCompatActivity {
+
+    public static final String EXTRA_DRINK_ID = "drinkId";
+    private ImageView photo;
+    private TextView name;
+    private TextView description;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink);
+        findActivityViews();
+
+        int drinkId = getIntent().getIntExtra(EXTRA_DRINK_ID, -1);
+        if (drinkId == -1) finish();
+
+        SQLiteOpenHelper openHelper = new DatabaseHelper(this);
+        try {
+            SQLiteDatabase db = openHelper.getReadableDatabase();
+            Cursor cursor = db.query("DRINK",
+                    new String[]{"NAME", "DESCRIPTION", "IMAGE_RESOURCE_ID"},
+                    "_id = ?",
+                    new String[]{Integer.toString(drinkId)},
+                    null, null, null);
+            if (cursor.moveToFirst()) {
+                name.setText(cursor.getString(0));
+                description.setText(cursor.getString(1));
+                photo.setImageResource(cursor.getInt(2));
+            }
+            cursor.close();
+            db.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Database unavailable!", Toast.LENGTH_LONG)
+                    .show();
+        }
+    }
+
+    private void findActivityViews() {
+        photo = findViewById(R.id.photo);
+        name = findViewById(R.id.name);
+        description = findViewById(R.id.description);
+    }
+}
---
+++ chapter17/Starbuzz/app/src/main/java/by/yakivan/starbuzz/database/DatabaseHelper.java
@@ -0,0 +1,59 @@
+package by.yakivan.starbuzz.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import by.yakivan.starbuzz.R;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final int DB_VERSION = 2;
+    public static final String DB_NAME = "starbuzz";
+
+    public DatabaseHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        updateDatabase(db, 0, DB_VERSION);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        updateDatabase(db, oldVersion, newVersion);
+    }
+
+    private void updateDatabase(SQLiteDatabase db, int oldVersion, int newVersion)  {
+        if (oldVersion < 1) {
+            db.execSQL("CREATE TABLE DRINK(" +
+                    "_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    "NAME TEXT," +
+                    "DESCRIPTION TEXT," +
+                    "IMAGE_RESOURCE_ID INTEGER)");
+            insertDrink(db, "Latte", "Espresso and steamed milk",
+                    R.drawable.latte);
+            insertDrink(db, "Cappuccino", "Espresso, hot milk and steamed-milk foam",
+                    R.drawable.cappuccino);
+            insertDrink(db, "Filter", "Our best drip coffee",
+                    R.drawable.filter);
+            insertDrink(db, "Tea", "Black tea help me to forget problems",
+                    R.drawable.tea);
+        }
+        if (oldVersion < 2) {
+            db.execSQL("ALTER TABLE DRINK ADD COLUMN FAVORITE NUMERIC;");
+        }
+    }
+
+    private static void insertDrink(SQLiteDatabase db,
+                                    String name,
+                                    String description,
+                                    int resourceId) {
+        ContentValues drink = new ContentValues();
+        drink.put("NAME", name);
+        drink.put("DESCRIPTION", description);
+        drink.put("IMAGE_RESOURCE_ID", resourceId);
+        db.insert("DRINK", null, drink);
+    }
+}
