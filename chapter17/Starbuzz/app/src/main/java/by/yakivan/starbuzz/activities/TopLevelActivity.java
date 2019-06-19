f5248b3181d34455bcc3eb3d330cf192c4f44203
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2019-06-19 20:43:17 +0300

    feat: #17 add updating the Favorite list after changes

--- chapter17/Starbuzz/app/src/main/java/by/yakivan/starbuzz/activities/TopLevelActivity.java
+++ chapter17/Starbuzz/app/src/main/java/by/yakivan/starbuzz/activities/TopLevelActivity.java
@@ -5,8 +5,8 @@ import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.CursorAdapter;
@@ -31,6 +31,46 @@ public class TopLevelActivity extends AppCompatActivity {
         findActivityViews();
         setupOptionsListView();
         setupFavoritesListView();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        try {
+            cursor = db.query("DRINK",
+                    new String[]{"_id", "NAME"},
+                    "FAVORITE = 1",
+                    null, null, null, null);
+            CursorAdapter cursorAdapter = (CursorAdapter) listFavorites.getAdapter();
+            cursorAdapter.changeCursor(cursor);
+        } catch (SQLException e) {
+            Toaster.toastLong(this, "Database unavailable");
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cursor.close();
+        db.close();
+    }
+
+    private void findActivityViews() {
+        listOptions = findViewById(R.id.list_options);
+        listFavorites = findViewById(R.id.list_favorites);
+    }
+
+    private void setupOptionsListView() {
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    Intent intent = new Intent(TopLevelActivity.this, DrinkCategoryActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        listOptions.setOnItemClickListener(itemClickListener);
     }
 
     private void setupFavoritesListView() {
@@ -60,23 +100,5 @@ public class TopLevelActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
-
-    private void setupOptionsListView() {
-        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if (position == 0) {
-                    Intent intent = new Intent(TopLevelActivity.this, DrinkCategoryActivity.class);
-                    startActivity(intent);
-                }
-            }
-        };
-        listOptions.setOnItemClickListener(itemClickListener);
-    }
-
-    private void findActivityViews() {
-        listOptions = findViewById(R.id.list_options);
-        listFavorites = findViewById(R.id.list_favorites);
     }
 }
