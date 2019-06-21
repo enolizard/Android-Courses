a12b0bbf8c6444c731b98164653da2b4f1edd85a
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2019-06-21 07:59:13 +0300

    feat: @HeadFirstAndroid #18 add simple service

--- HeadFirstAndroid/chapter18/Joke/app/src/main/java/by/yakivan/joke/MainActivity.java
+++ HeadFirstAndroid/chapter18/Joke/app/src/main/java/by/yakivan/joke/MainActivity.java
@@ -1,13 +1,29 @@
 package by.yakivan.joke;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findActivityViews();
+    }
+
+    private void findActivityViews() {
+        button = findViewById(R.id.button);
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(this, DelayedMessageService.class);
+        intent.putExtra(DelayedMessageService.EXTRA_MESSAGE,
+                getResources().getString(R.string.response));
+        startService(intent);
     }
 }
