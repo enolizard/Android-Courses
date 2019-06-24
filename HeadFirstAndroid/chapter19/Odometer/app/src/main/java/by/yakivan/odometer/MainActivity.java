47f1c0c7a0e1ecfa75653b235b6b807345da7c6f
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2019-06-24 11:32:12 +0300

    feat: @HeadFirstAndroid #19 add location permission request

--- HeadFirstAndroid/chapter19/Odometer/app/src/main/java/by/yakivan/odometer/MainActivity.java
+++ HeadFirstAndroid/chapter19/Odometer/app/src/main/java/by/yakivan/odometer/MainActivity.java
@@ -4,15 +4,21 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.os.Handler;
 import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    private final int PERMISSION_REQUEST_CODE = 698;
     private OdometerService odometer;
     private boolean bound = false;
     private TextView distanceView;
@@ -43,8 +49,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        Intent intent = new Intent(this, OdometerService.class);
-        bindService(intent, conn, Context.BIND_AUTO_CREATE);
+        if (ContextCompat.checkSelfPermission(this, OdometerService.PERMISSION_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            Intent intent = new Intent(this, OdometerService.class);
+            bindService(intent, conn, Context.BIND_AUTO_CREATE);
+        } else {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{OdometerService.PERMISSION_LOCATION},
+                    PERMISSION_REQUEST_CODE);
+
+        }
     }
 
     @Override
@@ -53,6 +68,22 @@ public class MainActivity extends AppCompatActivity {
         if (bound) {
             unbindService(conn);
             bound = false;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent(this, OdometerService.class);
+                    bindService(intent, conn, Context.BIND_AUTO_CREATE);
+                } else {
+                    Toast.makeText(this, "Please, turn on location permission", Toast.LENGTH_LONG)
+                            .show();
+                }
         }
     }
 
