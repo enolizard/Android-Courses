41232d89287f475b8dab921f43757857adcb30b9
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2019-06-25 07:12:39 +0300

    feat: @HeadFirstAndroid #19 add notification on failure permission request

--- HeadFirstAndroid/chapter19/Odometer/app/src/main/java/by/yakivan/odometer/MainActivity.java
+++ HeadFirstAndroid/chapter19/Odometer/app/src/main/java/by/yakivan/odometer/MainActivity.java
@@ -1,14 +1,19 @@
 package by.yakivan.odometer;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,7 +23,8 @@ import android.widget.Toast;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    private final int PERMISSION_REQUEST_CODE = 698;
+    private static final String CHANNEL_ID = "Odometer";
+    private static final int PERMISSION_REQUEST_CODE = 698;
     private OdometerService odometer;
     private boolean bound = false;
     private TextView distanceView;
@@ -81,6 +87,26 @@ public class MainActivity extends AppCompatActivity {
                     Intent intent = new Intent(this, OdometerService.class);
                     bindService(intent, conn, Context.BIND_AUTO_CREATE);
                 } else {
+                    createNotificationChannel();
+
+                    NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                            .setSmallIcon(android.R.drawable.ic_menu_compass)
+                            .setContentTitle("Odomteter")
+                            .setContentText("Location permission required")
+                            .setPriority(NotificationCompat.PRIORITY_HIGH)
+                            .setVibrate(new long[]{0, 1000})
+                            .setAutoCancel(true);
+                    Intent actionIntent = new Intent(this, MainActivity.class);
+                    PendingIntent actionPendingIntent = PendingIntent.getActivity(
+                            this,
+                            0,
+                            actionIntent,
+                            PendingIntent.FLAG_UPDATE_CURRENT);
+                    builder.setContentIntent(actionPendingIntent);
+
+                    NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                    notificationManager.notify(43, builder.build());
+
                     Toast.makeText(this, "Please, turn on location permission", Toast.LENGTH_LONG)
                             .show();
                 }
@@ -109,5 +135,18 @@ public class MainActivity extends AppCompatActivity {
                 handler.postDelayed(this, 1000);
             }
         });
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
