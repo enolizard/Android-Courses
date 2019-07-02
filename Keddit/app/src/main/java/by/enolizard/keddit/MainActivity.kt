8f1d4d398c6b42465be55f4c60af386741daa4bc
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2019-07-02 10:36:31 +0300

    feat: @Keddit #02 change main layout and setup toolbar

--- Keddit/app/src/main/java/by/enolizard/keddit/MainActivity.kt
+++ Keddit/app/src/main/java/by/enolizard/keddit/MainActivity.kt
@@ -1,12 +1,49 @@
 package by.enolizard.keddit
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+    }
+
+    fun changeFragment(frag: Fragment, cleanStack: Boolean = false) {
+        val ft = supportFragmentManager.beginTransaction()
+        if (cleanStack) {
+            clearBackStack()
+        }
+        ft.setCustomAnimations(R.anim.abc_fade_in, R.anim.abc_fade_out,
+                R.anim.abc_popup_enter, R.anim.abc_popup_exit)
+        ft.replace(R.id.activity_base_content, frag)
+        ft.addToBackStack(null)
+        ft.commit()
+    }
+
+    fun clearBackStack() {
+        val manager = supportFragmentManager
+
+        if (manager.backStackEntryCount > 0) {
+            val first = manager.getBackStackEntryAt(0)
+            manager.popBackStack(first.id, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+        }
+    }
+
+    override fun onBackPressed() {
+        val manager = supportFragmentManager
+
+        if (manager.backStackEntryCount > 1)
+            manager.popBackStack()
+        else {
+            finish()
+        }
     }
 }
