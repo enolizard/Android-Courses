e63ffcec4c1801461ba41eed87eb3f37f9bd9806
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2019-07-03 11:45:58 +0300

    feat: @Keddit #03 add delegated properties (lazy)

--- Keddit/app/src/main/java/by/enolizard/keddit/NewsFragment.kt
+++ Keddit/app/src/main/java/by/enolizard/keddit/NewsFragment.kt
@@ -11,6 +11,10 @@ import kotlinx.android.synthetic.main.news_fragment.*
 
 class NewsFragment : Fragment() {
 
+    private val newsList by lazy {
+        news_list
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 //        return inflater.inflate(R.layout.news_fragment, container, false)
         return container?.inflate(R.layout.news_fragment)
@@ -19,7 +23,7 @@ class NewsFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        news_list.setHasFixedSize(true)
-        news_list.layoutManager = LinearLayoutManager(context)
+        newsList.setHasFixedSize(true)
+        newsList.layoutManager = LinearLayoutManager(context)
     }
 }
