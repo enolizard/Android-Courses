9bf1cb19f92e7592e036267636e74e6ba1d18c69
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2019-07-04 23:15:03 +0300

    feat: @Keddit #04 create RedditNewsItem data class

---
+++ Keddit/app/src/main/java/by/enolizard/keddit/commons/models/RedditNewsItem.kt
@@ -0,0 +1,10 @@
+package by.enolizard.keddit.commons.models
+
+data class RedditNewsItem(
+        val author: String,
+        val title: String,
+        val numComments: Int,
+        val created: Long,
+        val thumbnail: String,
+        val url: String
+)
