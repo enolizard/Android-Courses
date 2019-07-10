71167ab102d64296d2b991b8df799aa1ebaf4092
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2019-07-10 09:33:51 +0300

    feat: @Keddit #08 make classes Parcelables

--- Keddit/app/src/main/java/by/enolizard/keddit/commons/Models.kt
+++ Keddit/app/src/main/java/by/enolizard/keddit/commons/Models.kt
@@ -1,5 +1,7 @@
 package by.enolizard.keddit.commons
 
+import android.os.Parcel
+import android.os.Parcelable
 import by.enolizard.keddit.commons.adapter.AdapterConstants
 import by.enolizard.keddit.commons.adapter.ViewType
 
@@ -10,12 +12,74 @@ data class RedditNewsItem(
     val created: Long,
     val thumbnail: String,
     val url: String
-) : ViewType {
+) : ViewType, Parcelable {
+
     override fun getViewType() = AdapterConstants.NEWS
+
+    constructor(parcelIn: Parcel) : this(
+        parcelIn.readString()!!,
+        parcelIn.readString()!!,
+        parcelIn.readInt(),
+        parcelIn.readLong(),
+        parcelIn.readString()!!,
+        parcelIn.readString()!!
+    )
+
+    override fun writeToParcel(p0: Parcel, p1: Int) {
+        p0.writeString(author)
+        p0.writeString(title)
+        p0.writeInt(numComments)
+        p0.writeLong(created)
+        p0.writeString(thumbnail)
+        p0.writeString(url)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<RedditNewsItem> {
+        override fun createFromParcel(parcel: Parcel): RedditNewsItem {
+            return RedditNewsItem(parcel)
+        }
+
+        override fun newArray(size: Int): Array<RedditNewsItem?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
 
 data class RedditNews(
     val after: String,
     val before: String,
     val news: List<RedditNewsItem>
-)
+) : Parcelable {
+
+    constructor(parcelIn: Parcel) : this(
+        parcelIn.readString()!!,
+        parcelIn.readString()!!,
+        mutableListOf<RedditNewsItem>().apply {
+            parcelIn.readTypedList(this, RedditNewsItem.CREATOR)
+        }
+    )
+
+    override fun writeToParcel(p0: Parcel, p1: Int) {
+        p0.writeString(after)
+        p0.writeString(before)
+        p0.writeTypedList(news)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<RedditNews> {
+        override fun createFromParcel(parcel: Parcel): RedditNews {
+            return RedditNews(parcel)
+        }
+
+        override fun newArray(size: Int): Array<RedditNews?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
