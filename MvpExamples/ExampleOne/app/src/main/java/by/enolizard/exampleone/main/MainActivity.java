3f41dad501285f625fb2e373f0ce7062189b6649
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2019-07-19 12:21:35 +0300

    feat: MVP #ExampleOne init

---
+++ MvpExamples/ExampleOne/app/src/main/java/by/enolizard/exampleone/main/MainActivity.java
@@ -0,0 +1,84 @@
+package by.enolizard.exampleone.main;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import by.enolizard.exampleone.R;
+
+public class MainActivity extends AppCompatActivity implements MainView {
+
+    private RecyclerView recyclerView;
+    private ProgressBar progressBar;
+    private MainPresenter presenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        progressBar = findViewById(R.id.progress);
+        presenter = new MainPresenter(this, new FindItemsInteractor());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        presenter.onResume();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        presenter.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void showProgress() {
+        progressBar.setVisibility(View.VISIBLE);
+        recyclerView.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public void hideProgress() {
+        progressBar.setVisibility(View.INVISIBLE);
+        recyclerView.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void setItems(List<String> items) {
+        recyclerView.setAdapter(new MainAdapter(items, presenter::onItemClicked));
+    }
+
+    @Override
+    public void showMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+}
---
+++ MvpExamples/ExampleOne/app/src/main/java/by/enolizard/exampleone/main/MainView.java
@@ -0,0 +1,14 @@
+package by.enolizard.exampleone.main;
+
+import java.util.List;
+
+public interface MainView {
+
+    void showProgress();
+
+    void hideProgress();
+
+    void setItems(List<String> items);
+
+    void showMessage(String message);
+}
